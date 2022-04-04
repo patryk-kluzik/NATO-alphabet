b329ab4a0d7e9bb2392b92d2404efc47970d1020
patryk-kluzik <patrykkluzik99@gmail.com>
2022-04-04 10:38:19 +0100

    added exception handling so program doesn't crash if invalid data is entered

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
 import pandas
-#TODO 1. Create a dictionary in this format:
+
+# TODO 1. Create a dictionary in this format:
 # {"A": "Alfa", "B": "Bravo"}
 
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
 
-nato_alphabet = {row.letter:row.code for (index,row) in data.iterrows()}
+nato_alphabet = {row.letter: row.code for (index, row) in data.iterrows()}
 
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 
-word = input("Enter a word: ").upper()
+# TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 
-result = [nato_alphabet[character] for character in word]
+# ----- updating project to handle exceptions ----- #
 
-print(result)
+def spell_word():
+    word = input("Enter a word: ").upper()
+
+    try:
+        result = [nato_alphabet[character] for character in word]
+    except KeyError:
+        print("Sorry, text you entered is invalid. Please only use letters in the alphabet")
+        spell_word()
+    else:
+        print(result)
+
+
+spell_word()
